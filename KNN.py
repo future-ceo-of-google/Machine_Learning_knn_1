6b5a2b3534ba5a8dff044d4a94cb054e5ca8fbc4
Sathishkumar M <sathish-pt3311@sathish-pt3311.csez.zohocorpin.com>
2020-01-07 16:07:29 +0530

    Modified File

--- KNN.py
+++ KNN.py
@@ -4,12 +4,13 @@ s=np.random.uniform(100,200,150)
 t=np.random.uniform(150,250,150)
 s=list(s)
 t=list(t)
-#plt.plot(s,'o',color='silver')
-#plt.plot(t,'o',color='gold')
+plt.plot(s,'o',color='silver')
+plt.plot(t,'o',color='gold')
 dic={}
 dic['A']=s
 dic['B']=t
 td=int(input('Enter the test data:'))
+plt.plot(td,'*',color='aquamarine')
 distance=[]
 for key in dic:
     for i in range(120):
@@ -18,4 +19,4 @@ for key in dic:
         t+=s
         distance.append(t)       
 distance.sort()
-print('It belongs to the class: ',distance[0][len(distance[0])-1])
+print('It belongs to the class:',distance[0][len(distance[0])-1])
