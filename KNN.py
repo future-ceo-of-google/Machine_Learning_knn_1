0a4346eb02631efe212f3e11cc7ad2a3cdf2f934
future-ceo-of-google <48747735+future-ceo-of-google@users.noreply.github.com>
2023-12-31 22:24:10 +0530

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -2,21 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 s=np.random.uniform(100,200,150)
 t=np.random.uniform(150,250,150)
-s=list(s)
+s=list(s)#storing dynamically generated data
 t=list(t)
-plt.plot(s,'o',color='silver')
-plt.plot(t,'o',color='gold')
+plt.plot(s,'o',color='#ffff00')#labelling the data in the graph
+plt.plot(t,'o',color='silver')
 dic={}
-dic['A']=s
+dic['A']=s#mapping the data with labels
 dic['B']=t
-td=int(input('Enter the test data:'))
-plt.plot(td,'*',color='aquamarine')
 distance=[]
+td=int(input('Enter the test data:'))#test for a single input 
+plt.plot(td,'o',color='aqua')
 for key in dic:
     for i in range(120):
-        s=key
-        t=str(abs(dic[key][i]-td))
-        t+=s
-        distance.append(t)       
+        t=str(abs(dic[key][i]-td))+key
+        distance.append(t)
 distance.sort()
-print('It belongs to the class:',distance[0][len(distance[0])-1])
+k=int(input('Enter the key value:')) #value for the k-nearsest neighbour
+s=t=0
+for i in range(k):
+    if distance[i][len(distance[i])-1]=='A':
+      s+=1
+    else:
+      t+=1
+if s>t:
+    print("Data belongs to class A")
+else:
+    print("Data belongs to class B")
